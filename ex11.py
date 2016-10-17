839bfde87a6a2f6da4ba03c9250df85d5a937aa3
duanzw <duanzhongwei@jiachongs.com>
2016-10-17 22:08:11 +0800

    输入

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print "How old are you?",
+age = raw_input();
+print "How tall are you?",
+height = raw_input();
+print "How much do you weight?",
+heavy = raw_input();
+
+
+print "Age:%r" % age," Height:",height," Bound:",heavy
