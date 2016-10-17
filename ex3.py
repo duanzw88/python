e989c76d1ac67d0bb203eb80f0e53d99c7f73613
duanzw <duanzhongwei@jiachongs.com>
2016-10-17 14:10:51 +0800

    练习四则运算和浮点数，以及格式化输出

---
+++ ex3.py
@@ -0,0 +1,27 @@
+#-*- coding: utf-8 -*-
+print "I will now count mu chickens:"
+
+#计算25+30/6的结果
+print "Hens", 25 + 30 / 6           #Hens30
+#计算100-25*3%4的结果
+print "Roosters",100 - 25 * 3 % 4   # Roosters 99
+
+print "Now I will count the eggs:"
+
+print "%.2f" %(3 + 2 + 1 - 5 + 4.0 % 2 - 1 / 4 + 6) #7
+print "0x%X" %(3 + 2 + 1 - 5 + 4.0 % 2 - 1 / 4 + 6 + 8) #7
+print "7.0 / 4.0 = ",7.0 /4.0
+print "7 / 4 = ",7 / 4
+
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7                  # false
+
+print "What is 3 + 2?",3 + 2.0
+print "What is 5-7?",5 - 7
+
+print "Oh! that's why it's False"
+print "How about some more"
+
+print "Is it greater?",5 > -2
+print "Is it greater or equal?",5 >= -2
+print "Is it less or equal?",5 <= -2
